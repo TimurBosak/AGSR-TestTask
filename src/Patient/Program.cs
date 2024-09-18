bead9ab20943f65de61450a37ad51076100d4cae
TimurBosak <roadto5knet@gmail.com>
2024-09-18 16:35:18 +0300

    Added connection string for mssql database and code in program needed for it's to function well

--- src/Patient/Program.cs
+++ src/Patient/Program.cs
@@ -1,15 +1,27 @@
+using Microsoft.EntityFrameworkCore;
+using Patient.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection")
+    .Replace("|DataDirectory|", AppDomain.CurrentDomain.BaseDirectory);
+
+builder.Services.AddDbContext<PatientContext>(options =>
+    options.UseSqlServer(connectionString));
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    var dbContext = services.GetRequiredService<PatientContext>();
+    dbContext.Database.Migrate();
+}
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
