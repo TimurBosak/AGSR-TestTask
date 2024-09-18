78e5da4003279e8e07c27e9f4400d28c62c15f48
TimurBosak <roadto5knet@gmail.com>
2024-09-18 16:13:15 +0300

    added implementation for interfaces

---
+++ src/Patient.Repositories/Implementations/Repository.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Patient.Repositories.Interfaces;
+using System.Linq.Expressions;
+
+namespace Patient.Repositories.Implementations
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        protected DbContext dbContext;
+        protected DbSet<T> dbSet;
+
+
+        public Repository(DbContext context)
+        {
+            dbContext = context;
+            dbSet = context.Set<T>();
+        }
+
+
+        public async Task<T> GetByIdAsync(params object[] id)
+        {
+            return await dbSet.FindAsync(id);
+        }
+
+        public async Task<IReadOnlyCollection<T>> GetAllAsync()
+        {
+            return await GetQuery().ToListAsync();
+        }
+
+        public async Task<IReadOnlyCollection<T>> GetWhereAsync(Expression<Func<T, bool>> predicate)
+        {
+            return await GetQuery().Where(predicate).ToListAsync();
+        }
+
+        public void Add(T entity)
+        {
+            dbSet.Add(entity);
+        }
+
+        public void AddRange(IEnumerable<T> entities)
+        {
+            dbSet.AddRange(entities);
+        }
+
+        public void Remove(T entity)
+        {
+            dbSet.Attach(entity);
+            dbSet.Remove(entity);
+        }
+
+        public void Update(T entity)
+        {
+            dbSet.Attach(entity);
+            dbSet.Update(entity);
+        }
+
+
+        protected virtual IQueryable<T> GetQuery()
+        {
+            return dbSet;
+        }
+    }
+}
--- src/Patient.Repositories/Interfaces/IRepository.cs
+++ src/Patient.Repositories/Interfaces/IRepository.cs
@@ -4,21 +4,18 @@ namespace Patient.Repositories.Interfaces
 {
     public interface IRepository<T> where T : class
     {
-        public interface IRepository<T> where T : class
-        {
-            Task<T> GetByIdAsync(params object[] id);
+        Task<T> GetByIdAsync(params object[] id);
 
-            Task<IReadOnlyCollection<T>> GetAllAsync();
+        Task<IReadOnlyCollection<T>> GetAllAsync();
 
-            Task<IReadOnlyCollection<T>> GetWhereAsync(Expression<Func<T, bool>> predicate);
+        Task<IReadOnlyCollection<T>> GetWhereAsync(Expression<Func<T, bool>> predicate);
 
-            void Add(T entity);
+        void Add(T entity);
 
-            void AddRange(IEnumerable<T> entities);
+        void AddRange(IEnumerable<T> entities);
 
-            void Update(T entity);
+        void Update(T entity);
 
-            void Remove(T entity);
-        }
-    }
+        void Remove(T entity);
+     }
 }
