332d58b42ece6e57743b82b83f355660d867439c
TimurBosak <roadto5knet@gmail.com>
2024-09-18 16:28:34 +0300

    added context for patient database

---
+++ src/Patient.Repositories/PatientContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Patient.Repositories
+{
+    public class PatientContext : DbContext
+    {
+        public PatientContext(DbContextOptions<PatientContext> options)
+            : base(options)
+        {
+
+        }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Domain.Models.Patient>()
+                .Property(p => p.BirthDate)
+                .IsRequired();
+        }
+    }
+}
